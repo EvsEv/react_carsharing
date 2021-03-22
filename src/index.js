bba9c3b8a645b9b366898b2e303404016cface01
EvsEv <enevstigneev@gmail.com>
2021-03-22 12:31:05 +0400

    v1 added fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/App/App";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { App } from "./components/App/App";
+
+import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
